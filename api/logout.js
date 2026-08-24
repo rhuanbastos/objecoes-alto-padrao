81b07ebaf0ff21ab2c278e874544052776b0ebd4
rhuanbastos <rhuanbastos@aguiaconsultoriaimobiliaria.com>
2026-08-24 17:12:22 -0300

    Update logout.js

--- api/logout.js
+++ api/logout.js
@@ -1,7 +1,7 @@
 // api/logout.js
 // Encerra a sessão do usuário (apaga o cookie de login).
 
-const { cookieLogout } = require('./auth');
+const { cookieLogout } = require('../lib/auth');
 
 module.exports = async function handler(req, res){
   if (req.method !== 'POST') {
